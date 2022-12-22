76ef6d7a7d98df1c2bb8c54400e5a57777dee860
BilalMangrio <100990831+BilalMangrio@users.noreply.github.com>
2022-12-23 00:37:55 +0500

    Add files via upload

---
+++ app.py
@@ -0,0 +1,39 @@
+import numpy as np
+from flask import Flask, request, jsonify, render_template
+import pickle
+
+app = Flask(__name__)
+
+gb=pickle.load(open('gb.pickle','rb'))
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/depression', methods=['POST'])
+def depression_api_method():
+  
+  data = request.get_json()
+  array = data['questionScoresArray']
+  question_scores = np.array(array, ndmin=2)
+  #print(question_scores)
+  prediction = gb.predict(question_scores)
+  final_prediction=int(prediction[0])
+  if(final_prediction==0):
+    final_='you are normal '
+  elif(final_prediction==1):
+    final_='you have mild depression'
+  elif(final_prediction==2):
+    final_='you have moderate depression'
+  elif(final_prediction==3):
+    final_='you have severe depression'
+  else:
+    print('unfortunately we couldnt predict ')
+    
+  print(final_)
+  return  jsonify(final_prediction)
+
+
+
+if __name__ == "__main__":
+    app.run(debug=False)
