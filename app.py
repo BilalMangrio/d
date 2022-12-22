6ce2dc034feb8183cbdd93be27766d8cf9a816ab
BilalMangrio <100990831+BilalMangrio@users.noreply.github.com>
2022-12-23 04:05:13 +0500

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,10 @@ import pickle
 
 app = Flask(__name__)
 
-gb=pickle.load(open('gb.pickle','rb'))
+
+Depr=pickle.load(open('depression_model.pickle','rb'))
+Str=pickle.load(open('StressModel.pickle','rb'))
+Anx=pickle.load(open('AnxietyModel.pickle','rb'))
 
 @app.route('/')
 def home():
@@ -17,7 +20,7 @@ def depression_api_method():
   array = data['questionScoresArray']
   question_scores = np.array(array, ndmin=2)
   #print(question_scores)
-  prediction = gb.predict(question_scores)
+  prediction = Depr.predict(question_scores)
   final_prediction=int(prediction[0])
   if(final_prediction==0):
     final_='you are normal '
@@ -27,6 +30,52 @@ def depression_api_method():
     final_='you have moderate depression'
   elif(final_prediction==3):
     final_='you have severe depression'
+  else:
+    print('unfortunately we couldnt predict ')
+    
+  print(final_)
+  return  jsonify(final_prediction)
+
+@app.route('/stress', methods=['POST'])
+def stress_api():
+  
+  data = request.get_json()
+  array = data['questionScoresArray']
+  question_scores = np.array(array, ndmin=2)
+  #print(question_scores)
+  prediction = Str.predict(question_scores)
+  final_prediction=int(prediction[0])
+  if(final_prediction==0):
+    final_='you are normal '
+  elif(final_prediction==1):
+    final_='you have mild stress'
+  elif(final_prediction==2):
+    final_='you have moderate stress'
+  elif(final_prediction==3):
+    final_='you have severe stress'
+  else:
+    print('unfortunately we couldnt predict ')
+    
+  print(final_)
+  return  jsonify(final_prediction)
+
+@app.route('/anxiety', methods=['POST'])
+def anxiety_api():
+  
+  data = request.get_json()
+  array = data['questionScoresArray']
+  question_scores = np.array(array, ndmin=2)
+  #print(question_scores)
+  prediction = Anx.predict(question_scores)
+  final_prediction=int(prediction[0])
+  if(final_prediction==0):
+    final_='bilal are normal '
+  elif(final_prediction==1):
+    final_='bilal have mild anxiety'
+  elif(final_prediction==2):
+    final_='bilal have moderate anxiety'
+  elif(final_prediction==3):
+    final_='bilal have severe anxiety'
   else:
     print('unfortunately we couldnt predict ')
     
